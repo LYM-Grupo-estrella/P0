98fb6271255d40e1b7247cd4fdb542cca3467d48
karenfbc <k.fuentesb@uniandes.edu.co>
2023-08-30 12:07:41 -0500

    cargador archivos txt

--- Main.py
+++ Main.py
@@ -6,8 +6,16 @@ import Parser
 import Comandos_Robot as Comandos_Rob
 
 
-def cargar_programa_txt():
-    return None
+def cargar_programa_txt(nombre_archivo) -> list:
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            programa = archivo.readlines()
+            return programa
+    except FileNotFoundError:
+        print(f"El archivo '{nombre_archivo}' no fue encontrado.")
+        return []
+
+
 
 def mostrar_menu():
     print("1. Cargar archivo txt")
@@ -18,16 +26,18 @@ def iniciar_aplicacion():
 
     continuar = True
 
-    lexer = Lexer()
-    parser = Parser()
+    #lexer = Lexer()
+    #parser = Parser()
 
     while continuar:
         mostrar_menu()
         opcion_seleccionada = int(input("Por favor seleccione una opción: "))
         if opcion_seleccionada == 1:
             nombre_archivo = input("Ingresa el nombre del archivo del programa: ")
-            programa = cargar_programa_txt
+            programa = cargar_programa_txt(nombre_archivo)
+            print(programa)
 
+iniciar_aplicacion()
 
 
 
