2f4f29eea95462f06ef77c9307b41509ceb7868e
karenfbc <k.fuentesb@uniandes.edu.co>
2023-09-02 22:38:25 -0500

    lexer peque

--- Lexer.py
+++ Lexer.py
@@ -1,1 +1,16 @@
-
+def lexer(programa):
+    tokens = []
+    for cadena in programa.split():
+        if cadena in ['defVar', 'defProc', 'if', 'else', 'while', 'repeat']:
+            tokens.append(('KEYWORD', cadena))
+        elif cadena == '=':
+            tokens.append(('OPERATOR', cadena))
+        elif cadena.isdigit():
+            tokens.append(('CONSTANT', cadena))
+        elif cadena == ';':
+            tokens.append(('PUNCTUATOR', cadena))
+        elif cadena == ';':
+            tokens.append(('IDENTIFIER', cadena))
+        else:
+            tokens.append(('LITERAL', cadena))
+    return tokens
